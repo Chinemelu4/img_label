fd91c0b00ebf5edc9e8bd8ef98e1c661df6113bf
Ezinwanne Aka <79057350+Chinemelu4@users.noreply.github.com>
2022-12-31 12:35:40 +0100

    Add files via upload

--- sign2.py
+++ sign2.py
@@ -1,5 +1,8 @@
 import pickle
 from pathlib import Path
+import os
+from PIL import Image
+import random
 
 import pandas as pd  # pip install pandas openpyxl
 import streamlit as st  # pip install streamlit
@@ -31,123 +34,93 @@ credentials = {
             }            
         }
     }
-authenticator = stauth.Authenticate(credentials, "sales_dashboard", "auth", cookie_expiry_days=1)
+authenticator = stauth.Authenticate(credentials, "sales_dashboard", "auth", cookie_expiry_days=0)
 
-
+#BASE_PATH = st.text_input("num")
 name, authentication_status, username = authenticator.login("Login", "main")
+
 
 if authentication_status == False:
     st.error("Username/password is incorrect")
 
-if authesntication_status == None:
+if authentication_status == None:
     st.warning("Please enter your username and password")
 
+
+
+
 if authentication_status:
-    # ---- READ EXCEL ----
-    @st.cache
-    def get_data_from_excel():
-        df = pd.read_excel(
-            io="supermarkt_sales.xlsx",
-            engine="openpyxl",
-            sheet_name="Sales",
-            skiprows=3,
-            usecols="B:R",
-            nrows=1000,
-        )
-        # Add 'hour' column to dataframe
-        df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
-        return df
+    state = st.session_state
+    bp = st.text_input("PATH")
 
-    df = get_data_from_excel()
+    if (not bp):
+        st.info('The annotation will begin, once you entered the folder path.')
+        st.stop()
+    
+    BASE_PATH = bp
+    OPTIONS = ["Nystagmus", "Meniere's Disease", "BPPV"]
 
-    # ---- SIDEBAR ----
+    if "annotations" not in state:
+        state.annotations = {}
+        state.files = os.listdir(BASE_PATH)
+        state.current_file = state.files[0]
+
+
+    def annotate(label):
+        state.annotations[state.current_file] = label
+        if state.files:
+            state.current_file = random.choice(state.files)
+            state.files.remove(state.current_file)
+
+
+    st.header("Dataset annotation")
+
+    if state.files:
+        selected_file = state.current_file
+        filename = os.path.join(BASE_PATH, selected_file)
+        st.write(f"Current file: {selected_file}")
+        image = Image.open(filename)
+        st.image(image)
+
+        c = st.columns(len(OPTIONS))
+        for idx, option in enumerate(OPTIONS):
+            c[idx].button(f"{option}", on_click=annotate, args=(option,))
+
+    else:
+        st.info("Everything annotated.")
+
     authenticator.logout("Logout", "sidebar")
     st.sidebar.title(f"Welcome {name}")
-    st.sidebar.header("Please Filter Here:")
-    city = st.sidebar.multiselect(
-        "Select the City:",
-        options=df["City"].unique(),
-        default=df["City"].unique()
+
+    st.info(f"Annotated: {len(state.annotations)}, Remaining: {len(state.files)}")
+
+    st.sidebar.write([f"{k},{v}" for k, v in state.annotations.items()])
+
+
+
+    st.download_button(
+        "Download annotations as CSV",
+        "\n".join([f"{k},{v}" for k, v in state.annotations.items()]),
+        file_name="export.csv",
     )
+    
 
-    customer_type = st.sidebar.multiselect(
-        "Select the Customer Type:",
-        options=df["Customer_type"].unique(),
-        default=df["Customer_type"].unique(),
-    )
+    # ---- SIDEBAR ----
+    
 
-    gender = st.sidebar.multiselect(
-        "Select the Gender:",
-        options=df["Gender"].unique(),
-        default=df["Gender"].unique()
-    )
-
-    df_selection = df.query(
-        "City == @city & Customer_type ==@customer_type & Gender == @gender"
-    )
-
-    # ---- MAINPAGE ----
-    st.title(":bar_chart: Sales Dashboard")
-    st.markdown("##")
-
-    # TOP KPI's
-    total_sales = int(df_selection["Total"].sum())
-    average_rating = round(df_selection["Rating"].mean(), 1)
-    star_rating = ":star:" * int(round(average_rating, 0))
-    average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
-
-    left_column, middle_column, right_column = st.columns(3)
-    with left_column:
-        st.subheader("Total Sales:")
-        st.subheader(f"US $ {total_sales:,}")
-    with middle_column:
-        st.subheader("Average Rating:")
-        st.subheader(f"{average_rating} {star_rating}")
-    with right_column:
-        st.subheader("Average Sales Per Transaction:")
-        st.subheader(f"US $ {average_sale_by_transaction}")
-
-    st.markdown("""---""")
-
-    # SALES BY PRODUCT LINE [BAR CHART]
-    sales_by_product_line = (
-        df_selection.groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
-    )
-    fig_product_sales = px.bar(
-        sales_by_product_line,
-        x="Total",
-        y=sales_by_product_line.index,
-        orientation="h",
-        title="<b>Sales by Product Line</b>",
-        color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
-        template="plotly_white",
-    )
-    fig_product_sales.update_layout(
-        plot_bgcolor="rgba(0,0,0,0)",
-        xaxis=(dict(showgrid=False))
-    )
-
-    # SALES BY HOUR [BAR CHART]
-    sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
-    fig_hourly_sales = px.bar(
-        sales_by_hour,
-        x=sales_by_hour.index,
-        y="Total",
-        title="<b>Sales by hour</b>",
-        color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
-        template="plotly_white",
-    )
-    fig_hourly_sales.update_layout(
-        xaxis=dict(tickmode="linear"),
-        plot_bgcolor="rgba(0,0,0,0)",
-        yaxis=(dict(showgrid=False)),
-    )
-
-
-    left_column, right_column = st.columns(2)
-    left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
-    right_column.plotly_chart(fig_product_sales, use_container_width=True)
-
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+            width: 450px;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+            width: 500px;
+            margin-left: -500px;
+            }
+            </style>
+            """,
+            unsafe_allow_html=True)
 
     # ---- HIDE STREAMLIT STYLE ----
     hide_st_style = """
